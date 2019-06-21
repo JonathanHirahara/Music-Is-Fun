1058c2c31caa198d670213ecfd113bfa656e57c2
JonathanHirahara <jdhirahara@gmail.com>
2019-06-21 16:13:11 -0600

    oops

--- app/models/Song.js
+++ app/models/Song.js
@@ -2,7 +2,7 @@ export default class Song {
     constructor(song) {
         this.title = song.trackName
         //Change 250x250 if you want a larger image
-        this.albumArt = song.artworkUrl60.replace(/60x60/g, "80x80")
+        this.albumArt = song.artworkUrl60.replace(/60x60/g, "200x200")
         this.artist = song.artistName
         this.collection = song.collectionName
         this.price = song.collectionPrice
@@ -10,11 +10,11 @@ export default class Song {
     }
     get Template() {
         return `
-        <div class="col">
+        <div class="col ">
         <img src="${ this.albumArt}"</img>
-         <h4>Title: ${ this.title}</h4> 
-         <h4>Artist: ${ this.artist}</h4>
-         <h4>Collection: ${ this.collection}</h4 >
+         <h6>Title: ${ this.title}</h6> 
+         <h6>Artist: ${ this.artist}</h6>
+         <h6>Collection: ${ this.collection}</h6>
          <p> Price: ${ this.price}</p>
          <audio controls class="preview" src="${ this.preview}"></audio control>
          </div>
